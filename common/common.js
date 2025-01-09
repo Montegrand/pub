05ec41ec338bc09e5bf376cf8a2586a6b536c192
Montegrand <hpt0311@naver.com>
2025-01-09 14:30:56 +0900

    Update common.js

--- common/common.js
+++ common/common.js
@@ -17,3 +17,99 @@
         originalAddEventListener.call(this, type, listener, options);
     };
 })();
+
+
+/**
+ * 모바일에서 메뉴 클릭시 .active로 판별하여 높이 재정렬
+ * .depth>.depth_list>.depth_item>(.depth_text+.depth>.depth_list>...)
+ * 
+ * 몇번째 깊이까지 적용할지 파라미터 및 구현 추가 예정 예시) 마지막 파라미터(type Number)에 따라 depth n 아래까지만 적용 가능하게
+ * 
+ * @param {Element} $txt event target txt
+ * @param {Event} e event
+ */
+function MobileMenuActive($txt, e, depth=false){
+    let $wrap = $txt.nextElementSibling;
+
+    if($wrap) e.preventDefault();
+    else return;
+
+    let $depth1 = $txt.closest('.depth1'),
+        $depthListItems = $depth1.querySelectorAll('.depth, .depth_list, .depth_item'),
+        $depthArr = Array.from($depth1.querySelectorAll('.depth')).reverse(),
+        $itemArr = $depth1.querySelectorAll('.depth_item');
+
+    let $item = $txt.parentElement,
+        $list = $item.parentElement;
+
+    $depthArr.forEach($depth=>{
+        $depth.fromHei = $depth.offsetHeight;
+    });
+
+    $depthListItems.forEach($ele=>{
+        $ele.style.transition = 'none';
+    });
+
+    $itemArr.forEach($item=>{
+        $item.from = $item.classList.contains('active');
+    });
+
+    if($item.classList.contains('active')){
+        $item.classList.remove('active');
+        $item.to = false;
+        $item.querySelectorAll('.depth_item').forEach(item=>{
+            if(!item.classList.contains('active')) return;
+            item.classList.remove('active');
+            item.to = false;
+        });
+    }else{
+        [...$list.children].forEach(item=>{
+            if(item === $item) return;
+            if(!item.classList.contains('active')) return;
+            item.classList.remove('active');
+            item.to = false;
+            item.querySelectorAll('.depth_item').forEach(ele=>{
+                if(!ele.classList.contains('active')) return;
+                ele.classList.remove('active');
+                ele.to = false;
+            });
+        });
+        $item.classList.add('active');
+        $item.to = true;
+    };
+
+    $depthArr.forEach($depth=>{
+        $depth.style.height = 'auto';
+        if($depth.parentElement.to){
+            $depth.toHei = $depth.firstElementChild.offsetHeight;
+        }else{
+            $depth.toHei = 0;
+            $depth.style.height = 0;
+        };
+    });
+    $depthArr.forEach($depth=>{
+        $depth.style.height = $depth.fromHei+'px';
+    });
+    $itemArr.forEach($item=>{
+        if($item.from) $item.classList.add('active');
+        else $item.classList.remove('active');
+    });
+
+    setTimeout(()=>{
+        $depthListItems.forEach($ele=>{
+            $ele.removeAttribute('style');
+        });
+        $depthArr.forEach($depth=>{
+            $depth.style.height = $depth.fromHei+'px';
+        });
+        setTimeout(()=>{
+            $itemArr.forEach($item=>{
+                if($item.to) $item.classList.add('active');
+                else $item.classList.remove('active');
+            });
+            $depthArr.forEach($depth=>{
+                $depth.style.height = $depth.toHei + 'px';
+            });
+        });
+    });
+};
